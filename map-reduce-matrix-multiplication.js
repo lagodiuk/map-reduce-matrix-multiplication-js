474fbf5e34ae165a645de95d20174fd1fb22ec60
Yurii Lahodiuk <yura.lagodiuk@gmail.com>
2014-10-13 23:56:39 +0200

    Encapsulation of multiplication logic

--- map-reduce-matrix-multiplication.js
+++ map-reduce-matrix-multiplication.js
@@ -37,7 +37,82 @@ function mapReduce(input, map, reduce) {
     return output;
 }
 
-var input = [
+function multiplyMatrixesMapReduce(a, b, resultName) {
+    var map1 = function(row, emit) {
+        var parts = row.split(",");
+        var matrixName = parts[0];
+        var i = parts[1];
+        var j = parts[2];
+        if(matrixName == 'A') {
+            emit(j, row);
+        }
+        if(matrixName == 'B') {
+            emit(i, row);
+        }
+    }
+    
+    var reduce1 = function(key, values, output) {
+        var rowsA = [];
+        var rowsB = [];
+        for(var i in values) {
+            var row = values[i];
+            var parts = row.split(",");
+            var matrixName = parts[0];
+            if(matrixName == 'A') {
+                rowsA.push(row);
+            }
+            if(matrixName == 'B') {
+                rowsB.push(row);
+            }
+        }
+        
+        for(var a in rowsA) {
+            var rowA = rowsA[a];
+            var partsA = rowA.split(",");
+            
+            for(var b in rowsB) {
+                var rowB = rowsB[b];
+                var partsB = rowB.split(",");
+                
+                var valueC = parseFloat(partsA[3]) * parseFloat(partsB[3]);
+                output.push('C,' + partsA[1] + ',' + partsB[2] + ',' + valueC)
+            }
+        }
+    }
+    
+    var map2 = function(row, emit) {
+        var parts = row.split(",");
+        var i = parts[1];
+        var j = parts[2];
+        var key = i + ',' + j;
+        emit(key, row);
+    }
+    
+    var reduce2 = function(key, values, output) {
+        var sum = 0;
+        for(var i in values) {
+            var row = values[i];
+            var parts = row.split(",");
+            var value = parseFloat(parts[3]);
+            sum += value;
+        }
+        output.push(resultName + ',' + key + ',' + sum);
+    }
+    
+    var input = a.concat(b);
+    
+    var output1 = mapReduce(input, map1, reduce1);
+    
+    // console.log(JSON.stringify(output1, null, 4));
+    
+    var output2 = mapReduce(output1, map2, reduce2);
+    
+    // console.log(JSON.stringify(output2, null, 4));
+
+    return output2;
+}
+
+var a = [
     "A,0,1,1.0",
     "A,0,2,2.0",
     "A,0,3,3.0",
@@ -46,7 +121,10 @@ var input = [
     "A,1,1,6.0",
     "A,1,2,7.0",
     "A,1,3,8.0",
-    "A,1,4,9.0",
+    "A,1,4,9.0"
+];
+
+var b = [
     "B,0,1,1.0",
     "B,0,2,2.0",
     "B,1,0,3.0",
@@ -60,74 +138,8 @@ var input = [
     "B,3,2,11.0",
     "B,4,0,12.0",
     "B,4,1,13.0",
-    "B,4,2,14.0",
+    "B,4,2,14.0"
 ];
 
-var map1 = function(row, emit) {
-    var parts = row.split(",");
-    var matrixName = parts[0];
-    var i = parts[1];
-    var j = parts[2];
-    if(matrixName == 'A') {
-        emit(j, row);
-    }
-    if(matrixName == 'B') {
-        emit(i, row);
-    }
-}
-
-var reduce1 = function(key, values, output) {
-    var rowsA = [];
-    var rowsB = [];
-    for(var i in values) {
-        var row = values[i];
-        var parts = row.split(",");
-        var matrixName = parts[0];
-        if(matrixName == 'A') {
-            rowsA.push(row);
-        }
-        if(matrixName == 'B') {
-            rowsB.push(row);
-        }
-    }
-    
-    for(var a in rowsA) {
-        var rowA = rowsA[a];
-        var partsA = rowA.split(",");
-        
-        for(var b in rowsB) {
-            var rowB = rowsB[b];
-            var partsB = rowB.split(",");
-            
-            var valueC = parseFloat(partsA[3]) * parseFloat(partsB[3]);
-            output.push('C,' + partsA[1] + ',' + partsB[2] + ',' + valueC)
-        }
-    }
-}
-
-var output1 = mapReduce(input, map1, reduce1);
-
-console.log(JSON.stringify(output1, null, 4));
-
-var map2 = function(row, emit) {
-    var parts = row.split(",");
-    var i = parts[1];
-    var j = parts[2];
-    var key = i + ',' + j;
-    emit(key, row);
-}
-
-var reduce2 = function(key, values, output) {
-    var sum = 0;
-    for(var i in values) {
-        var row = values[i];
-        var parts = row.split(",");
-        var value = parseFloat(parts[3]);
-        sum += value;
-    }
-    output.push('C,' + key + ',' + sum);
-}
-
-var output2 = mapReduce(output1, map2, reduce2);
-
-console.log(JSON.stringify(output2, null, 4));
+var c = multiplyMatrixesMapReduce(a, b, 'C');
+console.log(JSON.stringify(c, null, 4));
