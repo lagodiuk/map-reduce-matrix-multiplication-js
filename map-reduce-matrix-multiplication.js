2eeee934d74a3eef9c6bd2428ce8736efd2086a6
Yurii Lahodiuk <yura.lagodiuk@gmail.com>
2014-10-19 22:43:20 +0200

    Refcatoring + ability to configure separator and output

--- map-reduce-matrix-multiplication.js
+++ map-reduce-matrix-multiplication.js
@@ -1,73 +1,78 @@
-function multiplyMatrixesMapReduce(a, b, resultName) {
+function multiplyMatrixesMapReduce(a, b, config) {
+    var resultName = config['resultName'] ? config['resultName'] : 'output';
+    var separator = config['separator'] ? config['separator'] : ',';
+
     var map1 = function(row, emit) {
-        var parts = row.split(",");
+        var parts = row.split(separator);
         var matrixName = parts[0];
         var i = parts[1];
         var j = parts[2];
-        if(matrixName == 'A') {
+        if (matrixName == 'A') {
             emit(j, row);
         }
-        if(matrixName == 'B') {
+        if (matrixName == 'B') {
             emit(i, row);
         }
     }
-    
+
     var reduce1 = function(key, values, output) {
         var rowsA = [];
         var rowsB = [];
-        for(var i in values) {
+        for (var i in values) {
             var row = values[i];
-            var parts = row.split(",");
+            var parts = row.split(separator);
             var matrixName = parts[0];
-            if(matrixName == 'A') {
+            if (matrixName == 'A') {
                 rowsA.push(row);
             }
-            if(matrixName == 'B') {
+            if (matrixName == 'B') {
                 rowsB.push(row);
             }
         }
-        
-        for(var a in rowsA) {
+
+        for (var a in rowsA) {
             var rowA = rowsA[a];
-            var partsA = rowA.split(",");
-            
-            for(var b in rowsB) {
+            var partsA = rowA.split(separator);
+            var valueA = parseFloat(partsA[3]);
+
+            for (var b in rowsB) {
                 var rowB = rowsB[b];
-                var partsB = rowB.split(",");
-                
-                var valueC = parseFloat(partsA[3]) * parseFloat(partsB[3]);
-                output.push('C,' + partsA[1] + ',' + partsB[2] + ',' + valueC)
+                var partsB = rowB.split(separator);
+                var valueB = parseFloat(partsB[3]);
+
+                var valueC = valueA * valueB;
+                output.push(resultName + separator + partsA[1] + separator + partsB[2] + separator + valueC)
             }
         }
     }
-    
+
     var map2 = function(row, emit) {
-        var parts = row.split(",");
+        var parts = row.split(separator);
         var i = parts[1];
         var j = parts[2];
-        var key = i + ',' + j;
+        var key = i + separator + j;
         emit(key, row);
     }
-    
+
     var reduce2 = function(key, values, output) {
         var sum = 0;
-        for(var i in values) {
+        for (var i in values) {
             var row = values[i];
-            var parts = row.split(",");
+            var parts = row.split(separator);
             var value = parseFloat(parts[3]);
             sum += value;
         }
-        output.push(resultName + ',' + key + ',' + sum);
+        output.push(resultName + separator + key + separator + sum);
     }
-    
+
     var input = a.concat(b);
-    
+
     var output1 = mapReduce(input, map1, reduce1);
-    
+
     // console.log(JSON.stringify(output1, null, 4));
-    
+
     var output2 = mapReduce(output1, map2, reduce2);
-    
+
     // console.log(JSON.stringify(output2, null, 4));
 
     return output2;
